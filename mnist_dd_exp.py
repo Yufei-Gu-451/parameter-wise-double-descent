2fd27dd3d7100117406d746fd00fce811e41e911
Yufei-Gu-451 <yufei.gu.job@gmail.com>
2023-06-06 13:53:45 +0800

    Running with CUDA

--- mnist_dd_exp.py
+++ mnist_dd_exp.py
@@ -8,20 +8,23 @@ import numpy as np
 
 #------------------------------------------------------------------------------------------
 
+
 # Training Settings
 weight_reuse = False
 lr_decay = False
+#hidden_units = [1, 50, 100]
 hidden_units = [1, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 90, 100, 120, 150, 200, 400, 700, 1000]
-n_epochs = 6000
+n_epochs = 4000
 momentum = 0.95
 learning_rate = 0.01
 lr_decay_rate = 0.9
 sample_size = 4000
 
+
 if weight_reuse:
-    directory = "assets/mnist/weight-reuse-case/epoch=%d-2" % n_epochs
+    directory = "assets/mnist/weight-reuse-case/epoch=%d" % n_epochs
 else:
-    directory = "assets/mnist/standard-case/epoch=%d-2" % n_epochs
+    directory = "assets/mnist/standard-case/epoch=%d" % n_epochs
 
 output_file = os.path.join(directory, "epoch=%d.txt" % n_epochs)
 checkpoint_path = os.path.join(directory, "ckpt")
@@ -33,6 +36,42 @@ if not os.path.isdir(checkpoint_path):
 
 
 #------------------------------------------------------------------------------------------
+
+
+from torch.utils.data import DataLoader
+from prefetch_generator import BackgroundGenerator
+
+class DataLoaderX(DataLoader):
+    def __iter__(self):
+        return BackgroundGenerator(super().__iter__())
+
+
+# Return the trainloader and testloader of MINST
+def get_train_and_test_dataloader():
+    transform_train = transforms.Compose([
+        transforms.ToTensor(),
+    ])
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+    ])
+
+    trainset = datasets.MNIST(root='./data', train=True, download=True, transform = transform_train)
+
+    trainset_sub = torch.utils.data.Subset(trainset, indices = np.arange(4000))
+
+    trainloader = DataLoaderX(trainset_sub, batch_size=64, shuffle=True, num_workers=4, pin_memory=False)
+
+    testset = datasets.MNIST(root='./data', train=False, download=True, transform = transform_test)
+
+    testloader = DataLoaderX(testset, batch_size=64, shuffle=False, num_workers=4, pin_memory=False)
+
+    print('Load MINST dataset success;')
+    return trainloader, testloader
+
+
+#------------------------------------------------------------------------------------------
+
 
 class simple_FC(nn.Module):
     def __init__(self, n_hidden):
@@ -51,32 +90,6 @@ class simple_FC(nn.Module):
         return out
 
 
-# Return the trainloader and testloader of MINST
-def get_train_and_test_dataloader():
-    transform_train = transforms.Compose([
-        transforms.ToTensor(),
-    ])
-
-    transform_test = transforms.Compose([
-        transforms.ToTensor(),
-    ])
-
-    trainset = datasets.MNIST(root='./data', train=True,
-                                            download=True, transform = transform_train)
-    trainset_sub = torch.utils.data.Subset(trainset, indices = np.arange(4000))
-    trainloader = torch.utils.data.DataLoader(trainset_sub, batch_size=128,
-                                            shuffle=True, num_workers=4)
-
-    testset = datasets.MNIST(root='./data', train=False,
-                                        download=True, transform = transform_test)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=128,
-                                            shuffle=False, num_workers=4)
-
-    print('Load MINST dataset success;')
-
-    return trainloader, testloader
-
-
 # Set the neural network model to be used
 def get_model(hidden_unit):
     model = simple_FC(hidden_unit)
@@ -85,9 +98,6 @@ def get_model(hidden_unit):
     if hidden_unit == 1:
         torch.nn.init.xavier_uniform_(model.features[1].weight, gain=1.0)
         torch.nn.init.xavier_uniform_(model.classifier.weight, gain=1.0)
-    #elif hidden_unit > 30:
-    #    torch.nn.init.normal_(model.features[1].weight, mean=0.0, std=0.1)
-    #    torch.nn.init.normal_(model.classifier.weight, mean=0.0, std=0.1)
     else:
         torch.nn.init.normal_(model.features[1].weight, mean=0.0, std=0.1)
         torch.nn.init.normal_(model.classifier.weight, mean=0.0, std=0.1)
@@ -111,12 +121,15 @@ def get_model(hidden_unit):
     return model
 
 
+#------------------------------------------------------------------------------------------
+
+
 # Model Training
 def train(trainloader, model, optimizer, criterion):
     model.train()
     cumulative_loss, correct, total = 0.0, 0, 0
 
-    for inputs, labels in trainloader:
+    for idx, (inputs, labels) in enumerate(trainloader):
         # Calculate the training loss
         labels = torch.nn.functional.one_hot(labels, num_classes=10).float()
         inputs = inputs.to(device)
@@ -145,7 +158,7 @@ def test(testloader, model):
     cumulative_loss, correct, total = 0.0, 0, 0
 
     with torch.no_grad():
-        for inputs, labels in testloader:
+        for idx, (inputs, labels) in enumerate(testloader):
             # Calculate the testing loss
             labels = torch.nn.functional.one_hot(labels, num_classes=10).float()
             inputs = inputs.to(device)
@@ -170,33 +183,32 @@ def train_and_evaluate_model(trainloader, testloader, model, optimizer, criterio
     train_loss, train_acc, epoch = 0.0, 0.0, 0
 
     # Stops the training within the pre-set epoch size or when the model fits the training set (99%)
-    while epoch < n_epochs:
+    while epoch < min(n_epochs, hidden_unit * 20):
         # Perform weight decay before the interpolation threshold
         # LR decay by lr_decay_rate percent after every `500` epochs
-        if epoch > 1 and epoch % 500 == 1 and lr_decay:
+        if lr_decay and epoch > 1 and epoch % 500 == 1:
             optimizer.param_groups[0]['lr'] = optimizer.param_groups[0]['lr'] * lr_decay_rate
 
         # Train the model
         model, train_loss, train_acc = train(trainloader, model, optimizer, criterion)
 
-        # Print the status of current training and testing outcome
         epoch += 1
-        print("Epoch : %d ; Train Loss : %f ; Train Acc : %.3f ; LR : %.3f" 
-                  % (epoch, train_loss, train_acc, optimizer.param_groups[0]['lr']))
 
-        if hidden_unit < 30 and train_acc == 1:
-            break
+        # Print the status of current training and testing outcome
+        print("Epoch : %d ; Train Loss : %f ; Train Acc : %.3f" % (epoch, train_loss, train_acc))
+
 
     # Evaluate the model
     test_loss, test_acc = test(testloader, model)
 
-    state = {
-        'net': model.state_dict(),
-        'acc': test_acc,
-        'epoch': epoch,
-    }
-    torch.save(state, os.path.join(checkpoint_path, 'Simple_FC_%d.pth'%hidden_unit))
-    print("Torch saved successfully！");
+    if weight_reuse:
+        state = {
+            'net': model.state_dict(),
+            'acc': test_acc,
+            'epoch': epoch,
+        }
+        torch.save(state, os.path.join(checkpoint_path, 'Simple_FC_%d.pth'%hidden_unit))
+        print("Torch saved successfully！")
 
     return train_loss, train_acc, test_loss, test_acc
 
@@ -204,7 +216,10 @@ def train_and_evaluate_model(trainloader, testloader, model, optimizer, criterio
 if __name__ == '__main__':
     # Initialization
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    print('Using device : ', device)
+    print('Using device : ', torch.cuda.get_device_name(0))
+    print(torch.cuda.get_device_capability(0))
+
+    torch.backends.cudnn.benchmark = True
 
     # Get the training and testing data of specific sample size
     trainloader, testloader = get_train_and_test_dataloader()
@@ -224,11 +239,11 @@ if __name__ == '__main__':
                                     model, optimizer, criterion, hidden_unit)
 
         # Print training and evaluation outcome
-        print("\nHidden Neurons : %d ; Parameters : %d ; Train Loss : %f ; Train Acc : %.3f ; Test Loss : %f ; Test Acc : %.3f\n\n" \
-                % (hidden_unit, parameters, train_loss, train_acc, test_loss, test_acc))
+        print("\nHidden Neurons : %d ; Parameters : %d ; Train Loss : %f ; Train Acc : %.3f ; Test Loss : %f ; "
+              "Test Acc : %.3f\n\n" % (hidden_unit, parameters, train_loss, train_acc, test_loss, test_acc))
 
         # Write the training and evaluation output to file
         f = open(output_file, "a")
-        f.write("Hidden Neurons : %d ; Parameters : %d ; Train Loss : %f ; Train Acc : %.3f ; Test Loss : %f ; Test Acc : %.3f\n" \
-                % (hidden_unit, parameters, train_loss, train_acc, test_loss, test_acc))
+        f.write("Hidden Neurons : %d ; Parameters : %d ; Train Loss : %f ; Train Acc : %.3f ; Test Loss : %f ; "
+                "Test Acc : %.3f\n" % (hidden_unit, parameters, train_loss, train_acc, test_loss, test_acc))
         f.close()
